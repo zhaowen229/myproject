4db48cb9968e1fcc37526802ef41b9db9c81b5eb
zhaowen <zw_wy229@163.com>
2020-08-04 20:11:48 +0800

    learn ReentrantLock

---
+++ src/main/java/com/hello/thread/lock/MyReentrantLock2.java
@@ -0,0 +1,30 @@
+package com.hello.thread.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.hello.sync.DefinedThread2;
+
+/**
+ * 加锁方式和类锁一样
+ * 
+ * @see DefinedThread2
+ *
+ */
+public class MyReentrantLock2 {
+	// 静态变量
+	private static ReentrantLock lock = new ReentrantLock();
+
+	public void test() {
+		ReentrantLock lock = this.lock;
+		try {
+			lock.lock();
+			System.out.println(Thread.currentThread().getName() + "获得锁");
+		} catch (Exception e) {
+		}
+	}
+
+	public static void main(String[] args) {
+		new Thread(() -> new MyReentrantLock2().test()).start();
+		new Thread(() -> new MyReentrantLock2().test()).start();
+	}
+}
