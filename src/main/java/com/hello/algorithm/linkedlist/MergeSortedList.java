6d7b291ef1810a51ce2e8422ce081e013d706534
zhaowen <zw_wy229@163.com>
2020-09-07 16:50:46 +0800

    merge sorted linkedlist in iterator

--- src/main/java/com/hello/algorithm/linkedlist/MergeSortedList.java
+++ src/main/java/com/hello/algorithm/linkedlist/MergeSortedList.java
@@ -1,5 +1,8 @@
 package com.hello.algorithm.linkedlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MergeSortedList {
 
 	// leetcode:21 合并两个有序链表
@@ -18,12 +21,38 @@ public class MergeSortedList {
 
 	}
 
+	// leetcode:21 合并两个有序链表
+	public static ListNode mergeTwoListsIterator(ListNode l1, ListNode l2) {
+		ListNode prehead = new ListNode(-1);// 记录合并链表中的数据
+
+		ListNode prev = prehead;// prev 用来迭代
+		while (l1 != null && l2 != null) {
+			if (l1.val <= l2.val) {
+				prev.next = l1;
+				l1 = l1.next;
+			} else {
+				prev.next = l2;
+				l2 = l2.next;
+			}
+			prev = prev.next;
+			System.out.println(prev.val);// 如果返回prev结果是不对的,当前指向的是最后一个值。通过打印也可一看出来
+		}
+
+		// 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+		prev.next = l1 == null ? l2 : l1;
+		return prehead.next;
+
+	}
+
 	public static void printListNode(ListNode list) {
 		ListNode cur = list;
+		List<Integer> result = new ArrayList<>();
 		while (cur != null) {
-			System.out.println(cur.val);
+			result.add(cur.val);
 			cur = cur.next;
 		}
+		System.out.println(result.toString());
+
 	}
 
 	public static void main(String args[]) {
