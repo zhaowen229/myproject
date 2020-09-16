3aaeadfb83ed2927ea698120cc30d2d5863c61ca
zhaowen <zw_wy229@163.com>
2020-09-16 17:09:12 +0800

    tree

---
+++ src/main/java/com/hello/algorithm/tree/FindKthLargestNode.java
@@ -0,0 +1,70 @@
+package com.hello.algorithm.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉搜索树的第k大节点
+ * 
+ * https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+ * 
+ * 中序遍历：左 根 右
+ */
+public class FindKthLargestNode {
+	static List<Integer> list = new ArrayList<>();
+	static int count = 0;
+
+	public static int kthLargest(TreeNode root, int k) {
+		list = dfs(root, k);
+		return list.get(k - 1);
+	}
+
+	/**
+	 * 按中序遍历 倒序 把节点存到list中，想要得到的是：第k大的节点,那list中第k个元素（下标:k-1）就是要找的数
+	 * 
+	 * @param root
+	 * @param k
+	 * @return
+	 */
+	public static List<Integer> dfs(TreeNode root, int k) {
+		if (root != null) {
+			if (root.right != null) {
+				dfs(root.right, k);
+			}
+			list.add(root.val);
+			if (root.left != null) {
+				dfs(root.left, k);
+			}
+			count++;
+			if (count == k) {// 当count== K时 list中第k个元素已经得到了，其他就不需要再存到集合中了,返回list。
+				return list;
+			}
+		}
+		return list;
+
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(3);
+		root.left = new TreeNode(1);
+		root.right = new TreeNode(4);
+		root.left.right = new TreeNode(2);
+		System.out.println(kthLargest(root, 1));
+
+		middleOrderPrintNode(root);
+	}
+
+	/**
+	 * 打印中序遍历
+	 * 
+	 * @param root
+	 */
+	public static void middleOrderPrintNode(TreeNode root) {
+		if (root != null) {
+			middleOrderPrintNode(root.left);
+			System.out.println(root.val);
+			middleOrderPrintNode(root.right);
+		}
+	}
+
+}
---
+++ src/main/java/com/hello/algorithm/tree/N/PreOrderNTree.java
@@ -0,0 +1,105 @@
+package com.hello.algorithm.tree.N;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PreOrderNTree {
+	static List<Integer> res = new ArrayList<>();
+	int childrenSize = 0;
+
+	/**
+	 * recursion
+	 * 
+	 * @param root
+	 * @return
+	 */
+	public static List<Integer> preorder(Node root) {
+		if (root != null) {
+			res.add(root.val);
+			if (root.children != null) {
+				int size = root.children.size();
+				for (int i = 0; i < size; i++) {
+					preorder(root.children.get(i));
+				}
+			}
+		}
+		return res;
+	}
+
+	/**
+	 * iteration
+	 * 
+	 * 这里选用的是队列，其实可以直接使用Stack
+	 * 
+	 * @param root
+	 * @return
+	 */
+	public static List<Integer> preorderByIterator(Node root) {
+		List<Integer> res = new ArrayList<Integer>();
+		if (root != null) {
+			// Deque 双端队列
+			Deque<Node> queue = new LinkedList<>();
+			queue.offer(root);
+			while (!queue.isEmpty()) {
+				Node node = queue.pollLast();// 取最后一个元素（子节点最左边的节点）
+				res.add(node.val);
+				if (node.children != null) {
+					// 处理的关键：反转子节点，保证最左边的节点，在栈顶
+					Collections.reverse(node.children);
+					for (Node obj : node.children) {
+						queue.offer(obj);
+					}
+				}
+			}
+
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		// 构造N叉树
+		Node firstOf_FirstChildOfroot = new Node(5);
+		Node secondOf_FirstChildOfroot = new Node(6);
+
+		List<Node> list2 = new ArrayList<Node>();
+		list2.add(firstOf_FirstChildOfroot);
+		list2.add(secondOf_FirstChildOfroot);
+
+		Node FirstChildOfroot = new Node(3, list2);
+		Node secondChildOfroot = new Node(2);
+		Node thirdChildOfroot = new Node(4);
+
+		List<Node> list1 = new ArrayList<Node>();
+		list1.add(FirstChildOfroot);
+		list1.add(secondChildOfroot);
+		list1.add(thirdChildOfroot);
+
+		Node root = new Node(1, list1);
+
+		System.out.println("结果:" + preorder(root).toString());
+
+		System.out.println("结果:" + preorderByIterator(root));
+
+	}
+
+}
+
+class Node {
+	public int val;
+	public List<Node> children;
+
+	public Node() {
+	}
+
+	public Node(int _val) {
+		val = _val;
+	}
+
+	public Node(int _val, List<Node> _children) {
+		val = _val;
+		children = _children;
+	}
+}
