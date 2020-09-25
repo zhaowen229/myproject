465d6248b20dd6ea662e2b477f44ed3ab6743674
zhaowen <zw_wy229@163.com>
2020-09-25 15:33:33 +0800

    whether if it is a subsequence.

---
+++ src/main/java/com/hello/algorithm/IsSubsequence.java
@@ -0,0 +1,36 @@
+package com.hello.algorithm;
+
+public class IsSubsequence {
+
+	/**
+	 * 双指针
+	 */
+	public static boolean isSubsequence(String s, String t) {
+		char[] arrs = s.toCharArray();
+		char[] arrt = t.toCharArray();
+		int p = 0;
+		int q = 0;
+		while (p < arrs.length && q < arrt.length) {
+			if (arrs[p] == arrt[q]) {
+				p++;
+				q++;
+			} else {
+				q++;
+			}
+		}
+		return p == arrs.length;
+	}
+
+	/**
+	 * 逐个查找
+	 */
+	public boolean isSubsequence2(String s, String t) {
+		int index = -1;
+		for (char val : s.toCharArray()) {
+			index = t.indexOf(val, index + 1);
+			if (index == -1)
+				return false;
+		}
+		return true;
+	}
+}
