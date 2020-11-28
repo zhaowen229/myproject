cb4683f48f33eaa520d1c9f3e638c6a037fa96aa
zhaowen <zw_wy229@163.com>
2020-11-28 22:45:50 +0800

    modify singleton

--- src/main/java/com/hello/designmodel/single/SingleTon.java
+++ src/main/java/com/hello/designmodel/single/SingleTon.java
@@ -1,18 +1,19 @@
 package com.hello.designmodel.single;
 
 /**
- * 单例-饿汉式
- * 线程安全
- * 容易产生垃圾对象
+ * 单例-饿汉式 在获取对象之前，单例对象已经创建完成。
+ * 
+ * 线程安全 容易产生垃圾对象
+ * 
  * @author czhao1
  *
  */
 public class SingleTon {
 	private static SingleTon instance = new SingleTon();
-	
-	private SingleTon(){
+
+	private SingleTon() {
 	}
-	
+
 	public static SingleTon getInstance() {
 		return instance;
 	}
--- src/main/java/com/hello/designmodel/single/SingleTon2.java
+++ src/main/java/com/hello/designmodel/single/SingleTon2.java
@@ -1,15 +1,16 @@
 package com.hello.designmodel.single;
 
 /**
- * 单例-懒汉式
- * Lazy 初始化
- * 线程不安全
+ * 单例-懒汉式 在对象使用时，才去创建对象
+ * 
+ * Lazy 初始化 线程不安全
+ * 
  * @author czhao1
  *
  */
 public class SingleTon2 {
 	private static SingleTon2 instance = null;
-	
+
 	private SingleTon2() {
 	}
 
