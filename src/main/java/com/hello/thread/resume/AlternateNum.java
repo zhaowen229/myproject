9499b8296ba69f4d8adfcdafdcfaa7ed31731934
zhaowen <zw_wy229@163.com>
2021-12-12 21:53:12 +0800

    alternate print num

---
+++ src/main/java/com/hello/thread/resume/AlternateNum.java
@@ -0,0 +1,49 @@
+package com.hello.thread.resume;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AlternateNum {
+    private static Object lock = new Object();
+
+    private void printOddNumber(Integer num) {
+
+        synchronized (this) {
+            try {
+                this.notifyAll();
+                System.out.println(Thread.currentThread().getName()+" print num:" + num);
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void main(String... args) {
+        AlternateNum alternateNum = new AlternateNum();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i < 10; i=i+2) {
+                    alternateNum.printOddNumber(i);
+
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 2; i <= 10; i=i+2) {
+                    alternateNum.printOddNumber(i);
+                }
+            }
+        }).start();
+    }
+
+}
+
+
