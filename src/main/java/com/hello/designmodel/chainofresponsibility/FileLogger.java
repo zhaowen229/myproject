4780eca149d2dc7ad676d024e688378ea94e9131
zhaochunhui <zhaochunhui@bonree.com>
2022-09-13 16:12:40 +0800

    design model: chain of Responsibility

---
+++ src/main/java/com/hello/designmodel/chainofresponsibility/FileLogger.java
@@ -0,0 +1,13 @@
+package com.hello.designmodel.chainofresponsibility;
+
+public class FileLogger extends AbstractLogger{
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println("File Console::Logger: " + message);
+    }
+}
