f27ce11aadd5ecb951770fcd129338d29cb850f6
zhaochunhui <zhaochunhui@bonree.com>
2024-07-04 15:29:48 +0800

    feat alternate abc

---
+++ src/main/java/com/hello/thread/resume/AlternatePrintWay1.java
@@ -0,0 +1,87 @@
+package com.hello.thread.resume;
+
+/**
+ * @author zhaoc
+ * 交替打印abc
+ */
+public class AlternatePrintWay1 {
+    private static Object object = new Object();
+    private static int state = 0;
+
+    public static void main(String[] args) {
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    synchronized (object) {
+                        while (state % 3 != 0) {
+                            try {
+                                object.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        state++;
+                        System.out.println("A"+state);
+                        object.notifyAll();
+                        System.out.println("A");
+                    }
+
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    synchronized (object) {
+                        while (state % 3 != 1) {
+                            try {
+                                object.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        state++;
+                        System.out.println("B"+state);
+                        object.notifyAll();
+                        System.out.println("B");
+                    }
+
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    synchronized (object) {
+                        while (state % 3 != 2) {
+                            try {
+                                object.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        state++;
+                        System.out.println("C"+state);
+                        object.notifyAll();
+                        System.out.println("C");
+                    }
+
+                }
+            }
+        }).start();
+    }
+
+
+}
